f40fd3e046ed6a83964ac409dde3a1398bac06d0
Peter Bittner <peter@painless.software>
2017-12-27 18:07:02 +0100

    Group code blocks together for main execution flow
    Add transparency to bullet and cloud images
    
    Remove unused color definitions

--- heli_shooter.py
+++ heli_shooter.py
@@ -1,29 +1,9 @@
 import pygame
 import random
 
-Aqua = (0, 255, 255)
-Black = (0, 0, 0)
-Blue = (0, 0, 255)
-Fuchsia = (255, 0, 255)
-Gray = (128, 128, 128)
-Green = (0, 128, 0)
-Lime = (0, 255, 0)
-Maroon = (128, 0, 0)
-NavyBlue = (0, 0, 128)
-Olive = (128, 128, 0)
-Purple = (128, 0, 128)
-Red = (255, 0, 0)
-Silver = (192, 192, 192)
-Teal = (0, 128, 128)
-White = (255, 255, 255)
-Yellow = (255, 255, 0)
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode([400, 400])
-pygame.display.set_caption(" Shooter")
-clock = pygame.time.Clock()
-bg = pygame.image.load("bg.png")
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 
 def msg(txt, size, color, x, y):
@@ -51,12 +31,14 @@ class Player(pygame.sprite.Sprite):
     def shoot(self):
         now = pygame.time.get_ticks()
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_SPACE]:
             if now - self.last_shot > self.shot_delay:
                 self.last_shot = now
                 bullet = Bullet(self.rect.centerx, self.rect.top, 10, 0)
                 all_sprites.add(bullet)
                 bullets.add(bullet)
+
         if keys[pygame.K_LSHIFT]:
             if now - self.last_shot > self.shot_delay:
                 self.last_shot = now
@@ -91,9 +73,9 @@ class Player(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, vx, vy):
         super().__init__()
-        self.image = pygame.image.load("b.png")
+        self.image = pygame.image.load("bullet.png")
         self.image.convert_alpha()
-        self.image.set_colorkey(Blue)
+        self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -128,6 +110,7 @@ class Mob(pygame.sprite.Sprite):
             self.rect.x = 370
             self.rect.y = random.randrange(30, 250)
             self.vx = random.randrange(-6, -1)
+
         self.rect.x += -2
 
 
@@ -135,7 +118,7 @@ class Cloud(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("cloud.png")
-        self.image.set_colorkey(Blue)
+        self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 40
@@ -150,8 +133,8 @@ class Cloud(pygame.sprite.Sprite):
 class Ebullet(pygame.sprite.Sprite):
     def __init__(self, x, y, vx, vy):
         super().__init__()
-        self.image = pygame.image.load("b.png").convert_alpha()
-        self.image.set_colorkey(Blue)
+        self.image = pygame.image.load("bullet.png").convert_alpha()
+        self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -182,11 +165,11 @@ class Boat(pygame.sprite.Sprite):
         self.vy = 0
 
     def update(self):
-
         self.rect.x += self.vx
         if self.rect.left <= 0:
             self.kill()
             mobgen()
+
         now = pygame.time.get_ticks()
         if now - self.last_shot > 600:
             self.last_shot = now
@@ -231,6 +214,7 @@ class Ecopter(pygame.sprite.Sprite):
         if self.rect.x < 0:
             self.kill()
             mobgen()
+
         now = pygame.time.get_ticks()
         if now - self.last > 600:
             self.last = now
@@ -258,10 +242,8 @@ def newecopter():
 
 
 def newship():
-    now = pygame.time.get_ticks()
-    msg("!", 100, Red, 380, 200)
+    msg("!", 100, RED, 380, 200)
     pygame.display.update()
-    now = pygame.time.get_ticks()
     ship = Ship()
     all_sprites.add(ship)
     ships.add(ship)
@@ -281,14 +263,16 @@ def mobgen():
 
 def start():
     global hi_score, score
-    screen.fill(White)
 
-    msg("Heli Shooter", 50, Red, 200, 100)
+    screen.fill(WHITE)
+    msg("Heli Shooter", 50, RED, 200, 100)
+
     if score > hi_score:
         hscore = open("highscore.txt", "w")
         hscore.write(str(score))
         hscore.close()
-    msg("High Score:-" + str(hi_score), 20, Red, 200, 50)
+
+    msg("High Score:-" + str(hi_score), 20, RED, 200, 50)
 
     wait = True
     while wait:
@@ -296,32 +280,31 @@ def start():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
         c = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
         if 150 + 70 > c[0] > 150 and 160 + 40 > c[1] > 160:
-
-            msg("Start", 30, Blue, 180, 180)
+            msg("Start", 30, BLUE, 180, 180)
             if click[0] == 1:
                 wait = 0
         else:
-
-            msg("Start", 30, Red, 180, 180)
+            msg("Start", 30, RED, 180, 180)
 
         if 160 + 60 > c[0] > 160 and 230 + 40 > c[1] > 230:
-            msg("Exit", 30, Blue, 180, 240)
+            msg("Exit", 30, BLUE, 180, 240)
             if click[0] == 1:
                 pygame.quit()
                 quit()
         else:
-            msg("Exit", 30, Red, 180, 240)
+            msg("Exit", 30, RED, 180, 240)
+
         pygame.display.update()
 
 
 def pause():
     screen.blit(bg, [0, 0])
-    msg("Paused", 50, Red, 200, 100)
-
+    msg("Paused", 50, RED, 200, 100)
     pygame.display.update()
 
     wait = True
@@ -341,11 +324,12 @@ def pause():
 def Score():
     global intro, score, hscore
     gover = True
+
     if score > hi_score:
         hscore = open("highscore.txt", "w")
-
         hscore.write(str(score))
         hscore.close()
+
     while gover:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -361,29 +345,30 @@ def Score():
                 elif event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     quit()
-        msg("High Score :" + str(hi_score), 25, Blue, 200, 50)
-        msg("Game Over", 30, Red, 200, 100)
-        msg("Your Score :" + str(score), 25, Blue, 200, 200)
+        msg("High Score :" + str(hi_score), 25, BLUE, 200, 50)
+        msg("Game Over", 30, RED, 200, 100)
+        msg("Your Score :" + str(score), 25, BLUE, 200, 200)
         pygame.display.flip()
 
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode([400, 400])
+pygame.display.set_caption(" Shooter")
+clock = pygame.time.Clock()
+bg = pygame.image.load("bg.png")
+
 score = 0
+hscore = open("highscore.txt", "r")
+hi_score = int(hscore.read())
+
 run = True
 intro = True
 over = False
-hscore = open("highscore.txt", "r")
-hi_score = int(hscore.read())
+
 while run:
     clock.tick(50)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
 
-            player.shoot()
-
-            if event.key == pygame.K_RETURN:
-                pause()
     if intro:
         all_sprites = pygame.sprite.Group()
         cloud = Cloud()
@@ -426,46 +411,61 @@ while run:
         all_sprites.add(boat)
         over = False
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            player.shoot()
+            if event.key == pygame.K_RETURN:
+                pause()
+
     all_sprites.update()
     last_shot = pygame.time.get_ticks()
+
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     if hits:
         score += 1
         mobgen()
+
     hits1 = pygame.sprite.spritecollide(player, mobs, True)
     if hits1:
         Score()
         mobgen()
+
     hits2 = pygame.sprite.groupcollide(boats, bullets, True, True)
     if hits2:
         score += 3
-
         mobgen()
+
     hits4 = pygame.sprite.spritecollide(player, ebullets, True)
     if hits4:
         Score()
         over = True
+
     hits5 = pygame.sprite.groupcollide(bullets, ships, True, True)
     if hits5:
         score += 10
         mobgen()
+
     hits6 = pygame.sprite.spritecollide(player, ships, False)
     if hits6:
         Score()
         over = True
+
     hits7 = pygame.sprite.groupcollide(bullets, ecopters, True, True)
     if hits7:
         score += 5
         mobgen()
+
     hits8 = pygame.sprite.spritecollide(player, ecopters, False)
     if hits8:
         Score()
         over = True
-    screen.fill(White)
+
+    screen.fill(WHITE)
     screen.blit(bg, [0, 0])
     all_sprites.draw(screen)
-    msg("Score:" + str(score), 30, Red, 60, 30)
-
+    msg("Score:" + str(score), 30, RED, 60, 30)
     pygame.display.flip()
 
 pygame.quit()
